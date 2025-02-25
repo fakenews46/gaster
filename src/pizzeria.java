98b346ad475137cc0f4eba67a5076164192938b8
fakenews46 <2089467891@qq.com>
2025-02-25 11:38:07 +0800

    Slice-o-Heaven

--- src/pizzeria.java
+++ src/pizzeria.java
@@ -5,8 +5,53 @@ public class pizzeria {
     public String storeEmail;
     public String storeName;
     private String pizzaPrice;
+    private String pizzaIngredients;
+    private String sides;
+    private String drinks;
     private String orderID;
     private double orderTotal;
+
+    private static final String DEF_ORDER_ID = "DEF-SOH-099";
+    private static final String DEF_PIZZA_INGREDIENTS = "Mozzarella Cheese";
+    private static final double DEF_ORDER_TOTAL = 15.00;
+
+    public pizzeria() {
+        this.orderID = DEF_ORDER_ID;
+        this.pizzaIngredients = DEF_PIZZA_INGREDIENTS;
+        this.orderTotal = DEF_ORDER_TOTAL;
+        this.sides = "Garlic Bread";
+        this.drinks = "Coke";
+    }
+
+    public pizzeria(String orderID, String pizzaIngredients, double orderTotal) {
+        this.orderID = orderID;
+        this.pizzaIngredients = pizzaIngredients;
+        this.orderTotal = orderTotal;
+    }
+
+    public String getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(String orderID) {
+        this.orderID = orderID;
+    }
+
+    public double getOrderTotal() {
+        return orderTotal;
+    }
+
+    public void setOrderTotal(double orderTotal) {
+        this.orderTotal = orderTotal;
+    }
+
+    public String getPizzaIngredients() {
+        return pizzaIngredients;
+    }
+
+    public void setPizzaIngredients(String pizzaIngredients) {
+        this.pizzaIngredients = pizzaIngredients;
+    }
 
     public void takeOrder(String Price, String id, double total) {
         pizzaPrice = Price;
@@ -19,7 +64,7 @@ public class pizzeria {
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
-            System.out.println("Order is ready for pickup!");
+            System.out.println("Order preparation was interrupted!");
         }
 
         System.out.println("Your order is ready!");
@@ -32,7 +77,5 @@ public class pizzeria {
         System.out.println("Order ID: " + orderID);
         System.out.println("Order Total: " + orderTotal);
     }
-
-    
 }
 
